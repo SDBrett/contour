abf96df91e5ec0da74828888b7697d3817ae81c8
Brett Johnson <brett@sdbrett.com>
2019-11-29 14:21:30 +1100

    script fix

--- hack/travis/travis-check-files-changed.sh
+++ hack/travis/travis-check-files-changed.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # Checks if the files changed in the last commit are contained within specifed directories.
 # This is used to determine if a job should run based on changes within a commit
 # PATHS_TO_SEARCH can be a single path "site" or multipl paths "site hack", response if for any path specified.
@@ -9,16 +11,16 @@
 #      install:
 #        - ./hack/travis/travis-check-files-changed.sh $SEARCH_DIRECTORIES ; RETURN_CODE=$? ; if [ $RETURN_CODE -eq 137 ]; then travis_terminate 0; elif [ $RETURN_CODE -ne 0 ]; then travis_terminate $RETURN_CODE; fi
 
+set -e # halt script on error
 
 # 1. Get all the arguments of the script
 # https://unix.stackexchange.com/a/197794
-PATHS_TO_SEARCH="$*"
+
+PATHS_TO_SEARCH="$@"
 
 # 2. Make sure the paths to search are not empty
-if [ -z "$PATHS_TO_SEARCH" ]; then
-    echo "Please provide the paths to search for."
-    echo "Example usage:"
-    echo "./exit-if-path-not-changed.sh path/to/dir1 path/to/dir2"
+if [ $# -eq 0 ]; then
+    usage
     exit 1
 fi
 
@@ -26,7 +28,7 @@ fi
 LATEST_COMMIT=$(git rev-parse HEAD)
 
 # 4. Get the latest commit in the searched paths
-LATEST_COMMIT_IN_PATH=$(git log -1 --format=format:%H --full-diff $PATHS_TO_SEARCH)
+LATEST_COMMIT_IN_PATH=$(git log -1 --format=format:%H --full-diff "$@")
 
 if [ $LATEST_COMMIT != $LATEST_COMMIT_IN_PATH ]; then
     echo "Exiting this job because code in the following paths have not changed:"
